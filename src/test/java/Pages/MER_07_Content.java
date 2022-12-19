efb8806a75a2ba5414d1502be73dcb710d4082e9
schukru <kilicsukru@hotmail.com>
2022-12-19 21:30:16 +0300

    MER_7 Content was updated

--- src/test/java/Pages/MER_07_Content.java
+++ src/test/java/Pages/MER_07_Content.java
@@ -1,15 +1,9 @@
 package Pages;
 
-import Utilities.GWD;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-public class MER_07_Content extends Parent{
-
-    public MER_07_Content {
-        PageFactory.initElements(GWD.getDriver(), this);
-    }
+public class MER_07_Content extends Parent {
 
     @FindBy(xpath = "//button[text()='Accept all cookies']")
     private WebElement acceptCookies;
@@ -24,14 +18,30 @@ public class MER_07_Content extends Parent{
         switch (strElement) {
 
             case "writeName":
-                myElement = writeName;
+                //myElement = writeName;
                 break;
             case "rename":
-                myElement = rename;
+                //myElement = rename;
                 break;
 //            case "writeNameRenameit":myElement=writeNameRenameit;break;
 
         }
 
         sendKeysFunction(myElement, value);
+    }
+
+    @Override
+    public void findAndClick(String strElement) {
+
+    }
+
+    @Override
+    public void findAndContainsText(String strElement, String text) {
+
+    }
+
+    @Override
+    public void SearchAndDelete(String searchText) {
+
+    }
 }
