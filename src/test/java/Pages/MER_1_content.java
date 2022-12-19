5167b7a1e51d39ff486f5da596e0267d15de7db0
ahmet <oyuntaso@gmail.com>
2022-12-19 23:49:02 +0600

    mer_1

---
+++ src/test/java/Pages/MER_1_content.java
@@ -0,0 +1,64 @@
+package Pages;
+
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class MER_1_content extends Parent{
+    public MER_1_content(){
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+    @FindBy(css="[data-placeholder='Name']")
+    private WebElement SearcName;
+    @FindBy(xpath="//ms-edit-button//button")
+    private WebElement EditButton;
+
+    @FindBy(xpath = "//tbody/tr")
+    public List<WebElement> CitizenList;
+    @FindBy(xpath = "//*[contains(text(),'no data')]")
+    public WebElement bosList;
+
+
+
+    @Override
+    public void findAndSend(String strElement, String value) {
+        switch (strElement) {
+            case "SearcName": myElement = SearcName;break;
+
+        }
+        sendKeysFunction(myElement, value);
+    }
+    int cnt=0;
+
+    @Override
+    public void findAndClick(String strElement) {
+        switch (strElement) {
+            case "EditButton" : myElement =EditButton; break;
+
+        }
+        clickFunction(myElement);
+    }
+    @Override
+    public void findAndContainsText(String strElement, String text){
+        switch (strElement)
+        {
+            case "bosList" : myElement =bosList; break;
+
+        }
+
+        verifyContainsText(myElement,text);
+    }
+
+
+    @Override
+    public void SearchAndDelete(String searchText) {
+
+    }
+
+
+
+}
+
---
+++ src/test/java/StepDefinitions/MER_1.java
@@ -0,0 +1,97 @@
+package StepDefinitions;
+
+import Pages.CommonContent;
+import Pages.MER_1_content;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class MER_1 {
+    CommonContent cc = new CommonContent();
+    MER_1_content mr= new MER_1_content();
+
+    @Given("Click on the Citizenship tab")
+    public void clickOnTheCitizenshipTab() {
+    cc.findAndClick("setupOne");
+    cc.findAndClick("parameters");
+    cc.findAndClick("citizenShip");
+    }
+
+    @When("New country must be registered")
+    public void newCountryMustBeRegistered() {
+        cc.findAndClick("addButton");
+        cc.findAndSend("nameInput","tasoland");
+        cc.findAndSend("shortName","taso");
+        cc.findAndClick("saveButton");
+
+    }
+
+    @Then("Description success message should be displayed")
+    public void descriptionSuccessMessageShouldBeDisplayed() {
+        cc.findAndContainsText("successMessage","success");
+    }
+
+    @When("country registration must be changed")
+    public void countryRegistrationMustBeChanged() {
+        mr.findAndSend("SearcName","tasoland");
+        cc.findAndClick("searchButton");
+        mr.findAndClick("EditButton");
+        cc.findAndSend("nameInput","Altaykoy");
+        cc.findAndClick("saveButton");
+    }
+
+    @When("Search should be made with uppercase and lowercase letters.")
+    public void searchShouldBeMadeWithUppercaseAndLowercaseLetters() {
+        mr.findAndSend("SearcName","AlTaYKoY");
+        cc.findAndClick("searchButton");
+    }
+
+    @Then("verify that the country is listed on the screen")
+    public void verifyThatTheCountryIsListedOnTheScreen() {
+
+        mr.CitizenList.contains("Altaykoy");
+    }
+
+    @When("A registered country must be re-registered")
+    public void aRegisteredCountryMustBeReRegistered() {
+        cc.findAndClick("addButton");
+        cc.findAndSend("nameInput","Altaykoy");
+        cc.findAndSend("shortName","taso");
+        cc.findAndClick("saveButton");
+    }
+
+    @Then("Verify that the registration was unsuccessful")
+    public void verifyThatTheRegistrationWasUnsuccessful() {
+        cc.findAndContainsText("alreadyExist","exists");
+    }
+
+    @When("must be re-registered using a registered country spell and lowercase")
+    public void mustBeReRegisteredUsingARegisteredCountrySpellAndLowercase() {
+        cc.findAndClick("addButton");
+        cc.findAndSend("nameInput","AlTayKoy");
+        cc.findAndSend("shortName","TASO");
+        cc.findAndClick("saveButton");
+    }
+
+    @When("registered country must be deleted")
+    public void registeredCountryMustBeDeleted() {
+        mr.findAndSend("SearcName","AlTaYKoY");
+        cc.findAndClick("searchButton");
+        cc.findAndClick("deleteButton");
+        cc.findAndClick("deleteDialogBtn");
+
+    }
+    @When("-Non-existing country should be written in the search field.")
+    public void nonExistingCountryShouldBeWrittenInTheSearchField() {
+        mr.findAndSend("SearcName","AlTaYKoY");
+        cc.findAndClick("searchButton");
+    }
+
+
+    @Then("Verify that the country is not on the list")
+    public void verifyThatTheCountryIsNotOnTheList() {
+        mr.findAndContainsText("bosList","no data");
+    }
+}
