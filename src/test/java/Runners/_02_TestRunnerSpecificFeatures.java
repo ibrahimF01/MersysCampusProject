5f717157154d174dc8513a10700812b1345e9e0f
Huseyin YILMAZ <hsyilmaz1979@gmail.com>
2022-12-30 18:18:37 +0300

    .

--- src/test/java/Runners/_02_TestRunnerSpecificFeatures.java
+++ src/test/java/Runners/_02_TestRunnerSpecificFeatures.java
@@ -5,9 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles/MER_04_Discounts.feature",
         "src/test/java/FeatureFiles/MER_11_FieldSettings.feature"},
-        glue ={"StepDefinitions"},
-        dryRun=true // true olduğunda testi çalıştırma sadece featurelara ait steplerin varlığını kontrol et
-                      //false olduğunda ise testi çalıştırır. dryRun=true yazılı değilse default false olarak testleri çalıştırır
+        glue ={"StepDefinitions"}
+        //dryRun=true --> olduğunda testi çalıştırma sadece featurelara ait steplerin varlığını kontrol et
+        //dryRun=false -->olduğunda ise testi çalıştırır.
+        // yazılı değilse default false olarak testleri çalıştırır --> drtRun=false
 )
 
 public class _02_TestRunnerSpecificFeatures extends AbstractTestNGCucumberTests {
